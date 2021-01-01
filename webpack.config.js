434451f090fa5a8733ce38d92c65d40fa191dace
Hal Seki <hal@Hals-MacBook-Pro.local>
2021-01-01 15:49:47 +0900

    implements login-required swagger-ui

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './src/webpack/index.ts',
+  module: {
+    rules: [{
+        test: /\.ts$/,
+        use: ['ts-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(css)$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ]
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.ts'],
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Example API',
+      template: 'src/webpack/index.ejs',
+    }),
+  ],
+  performance: {
+    hints: false,
+  },
+};
