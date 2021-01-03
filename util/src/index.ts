608589d5f7a1b87214baaf928ccd69632e652336
Hal Seki <hal@Hals-MacBook-Pro.local>
2021-01-03 21:11:37 +0900

    command line tool

--- util/src/index.ts
+++ util/src/index.ts
@@ -1,11 +1,48 @@
 import {config} from './config';
-/*const poolData = {
-  UserPoolId: USER_POOL_ID, // your user pool ID
-  ClientId: USER_POOL_CLIENT_ID, // generated in the AWS console
-  Paranoia: PARANOIA_LEVEL // an integer between 1 - 10
-};
-const CognitoUserPoolWrapper = require('cognito-user-pool')(poolData);
-*/
+import commandLineArgs from 'command-line-args';
+import commandLineUsage from 'command-line-usage';
+
+const optionDefinitions = [
+  {
+    name: 'username',
+    alias: 'u',
+    type: String,
+    description: 'Username'
+  },
+  {
+    name: 'password',
+    alias: 'p',
+    type: String,
+    description: 'Password'
+  },
+  {
+    name: 'help',
+    alias: 'h',
+    type: Boolean,
+    description: 'show help'
+  }
+];
+
+const sections = [
+  {
+    header: 'Create AWS Cognito user',
+    content: 'Create AWS Cognito user for Remote Patient Monitoring system API.'
+  },
+  {
+    header: 'Options',
+    optionList: optionDefinitions
+  }
+];
+
+const options = commandLineArgs(optionDefinitions);
+if(options.help) {
+  const usage = commandLineUsage(sections);
+  console.log(usage);
+  process.exit(0);
+}
+
+console.log(options);
+
 function hello(name: string): string {
   return `Hello, ${name}!`;
 }
