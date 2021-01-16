6290e7e6265b207b9d56ba5b7192603b6f48b519
Hal Seki <hal@Hals-MacBook-Pro.local>
2021-01-17 00:01:24 +0900

    write center spec and nurse spec

--- test/e2e/fulltest.spec.ts
+++ test/e2e/fulltest.spec.ts
@@ -36,6 +36,9 @@ describe('admin user login', () => {
 describe('admin user', () => {
   let axios_admin: any;
   let center_id: string;
+  let center_id2: string;
+  let nurse_id: string;
+  let nurse_item: any;
   beforeAll(async () => {
     console.log('login as an admin')
     const ret = await axios.post(entry_point + '/api/admin/login', { username: secret.auth_user, password: secret.auth_pass });
@@ -66,6 +69,7 @@ describe('admin user', () => {
   it('create another center', async () => {
     const ret = await axios_admin.post(entry_point + '/api/admin/center', { centerName: 'B保健所' });
     expect(ret.data).toHaveProperty('centerId')
+    center_id2 = ret.data.centerId;
   })
 
   it('get two centers', async () => {
@@ -73,4 +77,38 @@ describe('admin user', () => {
     expect(ret.data.Count).toBe(2)
     expect(ret.data.Items).toHaveLength(2)
   })
+
+  it.skip('create new nurse to the center', async () => {
+    const ret = await axios_admin.post(entry_point + `/api/admin/center/${center_id}`, { nurseId: 'nurseA' });
+    expect(ret.data).toHaveProperty('nurseId')
+    expect(ret.data.manageCenters).toEqual(expect.arrayContaining(expect.objectContaining({ centerId: center_id })))
+    nurse_id = 'nurseA'
+  })
+
+  it.skip('read new nurse id', async () => {
+    pending
+    const ret = await axios_admin.get(entry_point + `/api/admin/nurse/${nurse_id}`);
+    expect(ret.data.manageCenters).toEqual(expect.arrayContaining(expect.objectContaining({ centerId: center_id })))
+  })
+
+  it.skip('create another nurse', async () => {
+    pending
+    const ret = await axios_admin.post(entry_point + `/api/admin/center/${center_id}`, { nurseId: 'nurseB' });
+    expect(ret.data).toHaveProperty('nurseId')
+    expect(ret.data.manageCenters).toEqual(expect.arrayContaining(expect.objectContaining({ centerId: center_id })))
+    nurse_item = ret.data
+  })
+
+  it.skip('get two nurses', async () => {
+    pending
+    const ret = await axios_admin.get(entry_point + `/api/admin/center/${center_id}/nurse`);
+    expect(ret.data.Count).toBe(2)
+    expect(ret.data.Items).toHaveLength(2)
+  })
+  it.skip('update existing nurse', async () => {
+    nurse_item.manageCenters[0].centerId = center_id2
+    const ret = await axios_admin.put(entry_point + `/api/admin/center/${center_id}`, nurse_item);
+    expect(ret.data.manageCenters[0].centerName).toBe('B保健所')
+  })
+
 })
