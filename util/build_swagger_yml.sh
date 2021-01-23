d21f14e0bf6de3dc027c6ce19e6913da31bb4f1f
Hal Seki <hal@Hals-MacBook-Pro.local>
2021-01-23 18:04:10 +0900

    add staging env

--- util/build_swagger_yml.sh
+++ util/build_swagger_yml.sh
@@ -2,6 +2,32 @@
 
 set -eu
 
+stg='dev'
+while getopts s-: opt; do
+  # OPTIND 番目の引数を optarg へ代入
+  optarg="${!OPTIND}"
+  [[ "$opt" = - ]] && opt="-$OPTARG"
+  case "-$opt" in
+    -s|--stage)
+      stg="$optarg"
+      shift
+      ;;
+    --)
+      break
+      ;;
+    -\?)
+      exit 1
+      ;;
+    --*)
+      echo "$0: illegal option -- ${opt##-}" >&2
+    exit 1
+      ;;
+  esac
+done
+shift $((OPTIND - 1))
+echo "create swagger config for $stg "
+
+
 PATH_DIR_SCRIPT=$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd)
 cd "$PATH_DIR_SCRIPT"
 
@@ -19,7 +45,9 @@ sed -e "s/COGNITO_NURSE_USER_POOL_ID_TO_BE_REPLACED/${NURSE_POOL_ID}/g" | \
 sed -e "s/COGNITO_PATIENT_USER_POOL_ID_TO_BE_REPLACED/${PATIENT_POOL_ID}/g" > ../src/swagger/_swagger.yaml
 
 cd ..
-./node_modules/.bin/swagger-merger -i ./src/swagger/_swagger.yaml -o ./templates/swagger.yaml
+tempdir=$(mktemp -d)
+./node_modules/.bin/swagger-merger -i ./src/swagger/_swagger.yaml -o ${tempdir}/swagger.yaml
+sed -e "s/#{SERVERLESS::Stage}/${stg}/g" ${tempdir}/swagger.yaml > ./templates/swagger.yaml
 
 # copy to swager-ui
 cp ./templates/swagger.yaml ./src/gh-swagger-ui/swagger.yaml
