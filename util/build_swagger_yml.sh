48ae8886a5c6fa5f058951f04486f0442f66fbc2
kE-sakai <19489493+kE-sakai@users.noreply.github.com>
2021-01-28 17:42:15 +0900

    remove processing to replace userpool

--- util/build_swagger_yml.sh
+++ util/build_swagger_yml.sh
@@ -5,21 +5,8 @@ set -eu
 PATH_DIR_SCRIPT=$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd)
 cd "$PATH_DIR_SCRIPT"
 
-ADMIN_POOL_ID='COGNITO_ADMIN_USER_POOL_ID'
-NURSE_POOL_ID='COGNITO_NURSE_USER_POOL_ID'
-PATIENT_POOL_ID='COGNITO_PATIENT_USER_POOL_ID'
-if [ -e ./config.json ]; then
-  ADMIN_POOL_ID=`cat ./config.json | jq -r '.cognito.adminUserPoolId'`
-  NURSE_POOL_ID=`cat ./config.json | jq -r '.cognito.nurseUserPoolId'`
-  PATIENT_POOL_ID=`cat ./config.json | jq -r '.cognito.patientUserPoolId'`
-fi
-
-sed -e "s/COGNITO_ADMIN_USER_POOL_ID_TO_BE_REPLACED/${ADMIN_POOL_ID}/g" ../src/swagger/swagger.yaml | \
-sed -e "s/COGNITO_NURSE_USER_POOL_ID_TO_BE_REPLACED/${NURSE_POOL_ID}/g" | \
-sed -e "s/COGNITO_PATIENT_USER_POOL_ID_TO_BE_REPLACED/${PATIENT_POOL_ID}/g" > ../src/swagger/_swagger.yaml
-
 cd ..
-./node_modules/.bin/swagger-merger -i ./src/swagger/_swagger.yaml -o ./templates/swagger.yaml
+./node_modules/.bin/swagger-merger -i ./src/swagger/swagger.yaml -o ./templates/swagger.yaml
 
 # copy to swager-ui
 cp ./templates/swagger.yaml ./src/gh-swagger-ui/swagger.yaml
