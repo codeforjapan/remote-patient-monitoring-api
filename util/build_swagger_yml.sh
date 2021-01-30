907471787c4ca04c09eb7ebe98dcc03744917234
Hal Seki <hal@Hals-MacBook-Pro.local>
2021-01-31 00:57:41 +0900

    fix build command

--- util/build_swagger_yml.sh
+++ util/build_swagger_yml.sh
@@ -32,10 +32,9 @@ PATH_DIR_SCRIPT=$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd)
 cd "$PATH_DIR_SCRIPT"
 
 cd ..
-# tempdir=$(mktemp -d)
-# ./node_modules/.bin/swagger-merger -i ./src/swagger/_swagger.yaml -o ${tempdir}/swagger.yaml
-# sed -e "s/#{SERVERLESS::Stage}/${stg}/g" ${tempdir}/swagger.yaml > ./templates/swagger.yaml
-./node_modules/.bin/swagger-merger -i ./src/swagger/swagger.yaml -o ./templates/swagger.yaml
+tempdir=$(mktemp -d)
+./node_modules/.bin/swagger-merger -i ./src/swagger/swagger.yaml -o ${tempdir}/swagger.yaml
+sed -e "s/#{SERVERLESS::Stage}/${stg}/g" ${tempdir}/swagger.yaml > ./templates/swagger.yaml
 
 # copy to swager-ui
 cp ./templates/swagger.yaml ./src/gh-swagger-ui/swagger.yaml
