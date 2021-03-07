7f1a0fa4fb1a90edb1f3415a5cdddc46c8226364
Hal Seki <hal@Hals-MacBook-Pro.local>
2021-03-07 14:58:57 +0900

    add login feature using refreshToken

--- src/lambda/admin.ts
+++ src/lambda/admin.ts
@@ -13,10 +13,10 @@ export namespace Admin {
         userPoolClientId: process.env.USER_POOL_CLIENT_ID!
       }
       const admin = new CognitoAdmin(config)
-      const res = await admin.signIn(bodyData.username, bodyData.password);
+      const res = (bodyData.refreshToken) ? await admin.refreshToken(bodyData.refreshToken) : await admin.signIn(bodyData.username, bodyData.password);
       return {
         statusCode: 200,
-        body: JSON.stringify({ username: bodyData.username, idToken: res?.AuthenticationResult?.IdToken!, RefreshToken: res?.AuthenticationResult?.RefreshToken! }),
+        body: JSON.stringify({ username: bodyData.username, idToken: res?.AuthenticationResult?.IdToken!, refreshToken: res?.AuthenticationResult?.RefreshToken! }),
       };
     } catch (err) {
       console.log("putAdminLogin error");
