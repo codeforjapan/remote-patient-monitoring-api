866c80e679c45ef06c5f2dbaa270398a306e304d
garasubo <garasubo@gmail.com>
2021-05-08 16:38:42 +0900

    fix open swagger script

--- util/open_swagger-ui.sh
+++ util/open_swagger-ui.sh
@@ -5,9 +5,14 @@ set -eu
 PATH_DIR_SCRIPT=$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd)
 cd "$PATH_DIR_SCRIPT"
 
-DISTRIBUTION=`cat ./config.json | jq -r '.distribution.SwaggerUIDistribution'`
+DISTRIBUTION=`cat ./config.json | jq -r '.[].distribution.SwaggerUIDistribution'`
 
 RET=`aws cloudfront get-distribution --id ${DISTRIBUTION}`
 URL=`echo $RET | jq -r '.Distribution.DomainName'`
-echo "open https://${URL}"
-open https://${URL}
+
+OPEN="open"
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    OPEN="xdg-open"
+fi
+echo "$OPEN https://${URL}"
+$OPEN https://${URL}
