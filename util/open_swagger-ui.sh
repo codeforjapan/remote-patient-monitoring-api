d6887380a1dd529a05d7e31bf028cc345ada7a98
Hal Seki <hal@Hals-MacBook-Pro.local>
2021-05-30 19:44:05 +0900

    add staging option

--- util/open_swagger-ui.sh
+++ util/open_swagger-ui.sh
@@ -2,10 +2,30 @@
 
 set -eu
 
+usage_exit() {
+        echo "Usage: $0 [-s stage]" 1>&2
+        exit 1
+}
+
+STAGE="dev"
+while getopts c:s:h OPT
+do
+    case $OPT in
+        s)  STAGE=$OPTARG
+            ;;
+        \?) usage_exit
+            ;;
+    esac
+done
+
+shift $((OPTIND - 1))
+
+echo $STAGE
+
 PATH_DIR_SCRIPT=$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd)
 cd "$PATH_DIR_SCRIPT"
 
-DISTRIBUTION=`cat ./config.json | jq -r '.[].distribution.SwaggerUIDistribution'`
+DISTRIBUTION=`cat ./config.json | jq -r "map(select(.apiGateway.stageName == \"${STAGE}\")) | .[].distribution.SwaggerUIDistribution"`
 
 RET=`aws cloudfront get-distribution --id ${DISTRIBUTION}`
 URL=`echo $RET | jq -r '.Distribution.DomainName'`
