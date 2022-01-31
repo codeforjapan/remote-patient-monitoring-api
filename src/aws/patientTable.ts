4be24dcf85e343d3a7c421d2f1b3fc58a6d25c71
Masaki Hidano <masagon@info-lounge.jp>
2022-01-31 20:28:11 +0900

    Revert "[WIP] add paging"
    This reverts commit f32a80c4593641ed29e52a8c57ddb4360c0a5b2d.

--- src/aws/patientTable.ts
+++ src/aws/patientTable.ts
@@ -13,22 +13,25 @@ export default class PatientTable {
     this.client = serviceClient;
   }
 
-  async getPatients(
+  getPatients(
     centerId: string
   ): Promise<DynamoDB.DocumentClient.ScanOutput | AWSError> {
     const params: DynamoDB.ScanInput = {
       TableName: process.env.PATIENT_TABLE_NAME!,
     };
-    const items: DynamoDB.DocumentClient.ItemList = [];
-    let result: DynamoDB.DocumentClient.ScanOutput, AWSError;
-    do {
-      result = await this.client.scan(params).promise()
-      items.push(...result.Items!)
-    } while (result.LastEvaluatedKey)
-    const filtered = items.filter(
-      (item) => item.centerId === centerId
-    );
-    return ({ Count: filtered.length, Items: filtered})
+    return new Promise((resolve, reject) => {
+      this.client.scan(params, (err, data) => {
+        if (err) {
+          console.log(err);
+          reject(err);
+        } else {
+          const filtered = data.Items!.filter(
+            (item) => item.centerId === centerId
+          );
+          resolve({ Count: filtered.length, Items: filtered });
+        }
+      });
+    });
   }
 
   getPatient(patientId: string): Promise<Patient | AWSError> {
